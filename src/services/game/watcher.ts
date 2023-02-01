46a0b44126d3775636ba215503ef8cb2c3c0ba15
Senior Engineer <69487052+seniortechdev@users.noreply.github.com>
2023-02-01 14:48:29 +0000

    create watcher service

---
+++ src/services/game/watcher.ts
@@ -0,0 +1,55 @@
+import { GameWatcher } from "./main";
+import { nowInUtc } from "@utils/index";
+import { differenceInMinutes } from "date-fns";
+
+class Watcher {
+  static initializedGame: GameWatcher;
+  static timeDelay: number = 1000 * 60 * 5;
+
+  static async watchGame(gameId: number, startTime: Date) {
+    const nowUCT = nowInUtc();
+    const timeDiff = differenceInMinutes(nowUCT, startTime);
+
+    console.log(
+      `${
+        timeDiff > 5
+          ? `Waiting for game with id ${gameId} to start`
+          : `Watching game with id ${gameId}`
+      }`
+    );
+
+    if (this.initializedGame && timeDiff < 5) {
+      this.initializedGame = new GameWatcher(gameId);
+      console.log(`Watching game with id ${gameId}`);
+    } else {
+      setTimeout(() => this.watchGame(gameId, startTime), this.timeDelay);
+    }
+
+    if (this.initializedGame) {
+      this.timeDelay = 1000 * 60 * 1;
+      this.initializedGame.insertPlays();
+    } else {
+      setTimeout(() => this.watchGame(gameId, startTime), this.timeDelay);
+    }
+
+    // exit if game has ended
+    if (this.initializedGame?.gameEnded) {
+      console.log(`Game ${gameId} has ended`);
+      process.exit(0);
+    }
+
+    // if game has not started, wait 5 minutes and try again
+    if (timeDiff < 5) {
+      setTimeout(() => this.watchGame(gameId, startTime), this.timeDelay);
+    }
+  }
+}
+
+const argv = process.argv;
+
+if (argv.length < 2) {
+  console.log("Please provide a game id and start time to watch");
+  process.exit(0);
+}
+
+Watcher.watchGame(parseInt(argv[2]), new Date(argv[3]));
